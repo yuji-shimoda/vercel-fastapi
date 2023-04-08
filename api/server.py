968ac718c5f3a041d70f48d8e64052741f729587
yuji.shimoda@gmail.com <shimoda.yuji@classmethod.jp>
2023-04-08 11:33:52 +0900

    feature: add routing.

--- api/server.py
+++ api/server.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from api.hello.views import router as hello_router
 
 app = FastAPI()
+api_router = APIRouter()
+api_router.include_router(hello_router, prefix="/hello", tags=["Hello"])
 
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return {"msg": "root"}
+
+
+app.include_router(api_router)
